641df351df65143ce0ab528e0dd3cc83f909e5fd
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-02-02 21:58:30 -0300

    01-Olá Mundo!

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSPHP: Iniciando um projeto</title>
+    <link rel="stylesheet" href="style.css"/>
+       
+</head>
+<body>
+
+<?php
+/**
+ * Bloco PHP com variável start 
+ * e comando de saída!
+ */
+$start = "Vamos Começar!";
+echo "<h1>Olá Mundo! {$start}</h1>";
+echo "<p id='js'>Loading...</p>";
+?>
+<script src="./assets/scripts.js"></script>   
+</body>
+</html>
